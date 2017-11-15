fd3f163fe1b2bca0ef4d3a777eb2568e072928c8
4010d9 <4010d9@mandarina.ece.uprm.edu>
2017-11-15 14:20:36 -0400

    Maked background gray and put 2 borders in it.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Christian Lopez");
+		JFrame myFrame = new JFrame("Christian J Lopez!");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
+	    MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
